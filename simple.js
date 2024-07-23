2029e6758888401e671ff15cd1d7594fe73ada40
Marvin Gith. <marvingithambo@gmail.com>
2024-07-23 05:18:15 +0300

    Using JS in html, just basics

---
+++ simple.js
@@ -0,0 +1,20 @@
+let name = "Marvin" + " " + "Githambo";
+let x = 56;
+y = 67;
+sum = x + y
+let mult = x *= y
+let division = x += y
+x;
+y;
+console.log(sum)
+let carName = "Mercedes" + " " + "Benz" + " " + "C550"
+let personalDetails = () => {
+    return {
+        firstName: "Marvin",
+        lastName: "Doe",
+        car: "Mercedes" + " " + "Benz",
+        age: 25
+    }
+}
+let details = personalDetails();
+console.log(details)
